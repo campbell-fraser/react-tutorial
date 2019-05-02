01174b27172837ebd003da918231e99be9d87615
Fraser Campbell <fraze860@gmail.com>
2019-05-02 16:25:34 +0100

    added entire react project

---
+++ my-app/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src="https://t2.ea.ltmcdn.com/en/images/9/4/7/img_how_to_train_a_shiba_inu_749_600.jpg" className="App-logo" alt="logo" />
+        <br></br>
+        <h1>dog spin</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
